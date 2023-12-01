d7d26e9b0010a75b13bb5d4600abaeff055cdab2
Beatriz Rodríguez <116880473+Bea-ro@users.noreply.github.com>
2023-12-01 11:13:38 +0100

    add pageDescriptions object

---
+++ src/data/pageDescriptions.js
@@ -0,0 +1,11 @@
+export const pageDescriptions = {
+  home: 'Me encantaría participar en el proceso de cambio de Madrid HiFi como Web Developer porque veo un doble reto: tanto desde el punto de vista de la programación, como desde el punto de vista del posicionamiento que la marca merece. Me encantan los retos, si son dobles mejor; y si además combinan mis dos pasiones...',
+  fullstack:
+    'Todo e-commerce requiere de una web Full Stack y Madrid HiFi necesita un Fronted acorde al posicionamiento de una marca consolidada de referencia, que trasmita sus fortalezas y ofrezca la mejor UX; así como un backend bien estructurado para lograr el mejor merchandising de un lineal de productos tan extenso.\nPor dar idea de cómo programo: utilizo herramientas como Eslint y Pretteier. Saco partido de hooks más allá de useState y useEffect y de librerías como React Hook Form y Styled Components. Se hacer web scraping con Puppeteer. Me gusta debugar y refactorizar código. He hecho testing con React Testing Library y Jest. Mis próximos retos: aprender Angular y UX (de momento...)',
+  fronted:
+    'Porque facilitar la UX y mejorar el diseño supone incrementar las ventas. En mis proyectos de Fronted, siempre tengo en cuenta detalles que mejoran la UX, no es suficiente con que todo funcione. Además, un buen desarrollo en términos de legibilidad y escalabilidad es básico cuando la base de datos es tan extensa y dinámica como en este e-commerce.',
+  backend:
+    'Porque tener una buena base de datos no solo facilita la mejor visualización de los productos, sino también un mejor análisis de ventas y consumer insights y un ahorro de costes de desarrollo, haciendo el e-commerce más dinámico y escalable.',
+  marketing:
+    'He trabajado más de diez años en Marketing en una marca líder en su categoría. Con un lineal de productos de más de dos mil referencias y casi cien puntos de venta, viví el lanzamiento del e-commerce, siendo partícipe tanto de dificultades como de logros, y del peso que enseguida tomó en el volumnen de ventas. Sin olvidar su relevancia en el posicionamiento de la marca.\nPor eso me encantaría tomar parte del reto al que se enfrenta Madrid HiFi.'
+}
--- src/pages/Marketing/Marketing.js
+++ src/pages/Marketing/Marketing.js
@@ -1,13 +1,12 @@
 import './Marketing.css'
+import { pageDescriptions } from '../../data/pageDescriptions'
 
 const main = document.querySelector('main')
 
 export const renderMarketing = () => {
   main.innerHTML = `
   <h1 class="category-title">Mi background en Marketing</h1>
-  <p class="description">He trabajado más de diez años en Marketing en una marca líder en su categoría. Con un lineal de productos de más de dos mil referencias y casi cien puntos de venta, viví el lanzamiento del e-commerce, siendo partícipe tanto de dificultades como de logros, y del peso que enseguida tomó en el volumnen de ventas. Sin olvidar su relevancia en el posicionamiento de la marca.</br>
-  Por eso me encantaría tomar parte del reto al que se enfrenta Madrid HiFi.
-  </p>
+  <p class="description">${pageDescriptions.marketing}</p>
 `
 
   main.id = 'marketing-main'
